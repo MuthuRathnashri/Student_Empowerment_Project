572617d5f9094531f8e6c6c9fcf7919ff0f3e307
Muthu Rathnashri <rathnashri2004@gmail.com>
2024-05-11 21:34:17 +0530

    commit

--- server/twilioService.js
+++ server/twilioService.js
@@ -2,7 +2,7 @@ const twilio = require('twilio');
 const Product = require('./Product');
 
 const accountSid = "AC10b5785cecac0bd43c5262f9c351f4f3";
-const authToken = "39f5a883758f372a96d76eff368ee664";
+const authToken = "4f081585cff4d9d4ef02acfb095bd855";
 const twilioClient = twilio(accountSid, authToken);
 
 // Function to send a message using Twilio
@@ -22,7 +22,7 @@ async function sendMessage(req, res) {
         const phoneNumber = '+91' + phone;
 
         // Construct the message using form data
-        const composedMessage = `Name: ${name}\nContact: ${contact}\nMessage: ${message}`;
+        const composedMessage = Name: ${name}\nContact: ${contact}\nMessage: ${message};
 
         // Send message using Twilio
         const messageResponse = await twilioClient.messages.create({
